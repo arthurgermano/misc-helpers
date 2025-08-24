ff3a28eb665c5e7596c9acf1eb17d58da444763f
Arthur Germano <arthurgermano@gmail.com>
2025-08-23 21:14:38 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
-// 1. Define a estrutura aninhada principal, que será a exportação padrão.
-const miscHelpers = {
-  auth: {
-    webAuthn: require("./auth/webauthn/index.js"),
-  },
-  constants: require("./constants.js"),
-  crypto: require("./crypto/index.js"),
-  custom: {
-    db: {
-      sequelize: {
-        setConditionBetweenDates: require("./custom/db/sequelize/setConditionsBetweenDates.js"),
-        setConditionBetweenValues: require("./custom/db/sequelize/setConditionsBetweenValues.js"),
-        setConditionStringLike: require("./custom/db/sequelize/setConditionStringLike.js"),
-      },
+// src/index.js (Versão Final e Explícita)
+
+// 1. Importa todas as suas sub-bibliotecas
+const auth = { webAuthn: require("./auth/webauthn/index.js") };
+const constants = require("./constants.js");
+const crypto = require("./crypto/index.js");
+const helpers = require("./helpers/index.js");
+const utils = require("./utils/index.js");
+const validators = require("./validators/index.js");
+const custom = {
+  db: {
+    sequelize: {
+      setConditionBetweenDates: require("./custom/db/sequelize/setConditionsBetweenDates.js"),
+      setConditionBetweenValues: require("./custom/db/sequelize/setConditionsBetweenValues.js"),
+      setConditionStringLike: require("./custom/db/sequelize/setConditionStringLike.js"),
     },
-    waitPlugin: require("./custom/waitPlugin"),
-    bulkProcessor: require("./custom/bulkProcessor.js"),
   },
-  helpers: require("./helpers/index.js"),
-  utils: require("./utils/index.js"),
-  validators: require("./validators/index.js"),
+  waitPlugin: require("./custom/waitPlugin"),
+  bulkProcessor: require("./custom/bulkProcessor.js"),
 };
 
-// 2. Anexa as funções de nível superior ao objeto principal para acesso direto.
-Object.assign(miscHelpers, miscHelpers.helpers);
-Object.assign(miscHelpers, miscHelpers.utils);
-Object.assign(miscHelpers, miscHelpers.validators);
-Object.assign(miscHelpers, miscHelpers.crypto);
-miscHelpers.bulkProcessor = miscHelpers.custom.bulkProcessor;
-miscHelpers.waitPlugin = miscHelpers.custom.waitPlugin;
+// 2. Exporta um único objeto que contém TUDO
+// As funções de nível superior são espalhadas (spread) aqui,
+// o que torna explícito para o `tsup` que elas são exportações nomeadas.
+module.exports = {
+  // Funções de acesso direto
+  ...helpers,
+  ...utils,
+  ...validators,
+  ...crypto,
+  bulkProcessor: custom.bulkProcessor,
+  waitPlugin: custom.waitPlugin,
 
-// Isso copia todas as propriedades de `miscHelpers` para `module.exports`,
-// criando tanto as exportações nomeadas quanto a padrão.
-Object.assign(module.exports, miscHelpers);
+  // Estrutura aninhada para acesso organizado
+  auth,
+  constants,
+  crypto,
+  custom,
+  helpers,
+  utils,
+  validators,
+};
